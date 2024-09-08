3ee680de966221c5b72cfde0f88aa1d103327f16
Hamza khalid <156505883+hazmaalirehmani@users.noreply.github.com>
2024-09-08 10:20:17 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
+import Contacts from './components/Contacts';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+ <>
+ <Navbar/>
+ <Home/>
+ <About/>
+ <Portfolio/>
+ <Experience/>
+ <Contacts/>
+ <Footer/>
+ </>
+  );
+}
+
+export default App;
